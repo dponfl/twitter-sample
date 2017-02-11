8f3744d9f261bf0786f3964fccd5230f69005b17
dmsch <dschukin@mail.ru>
2017-02-11 19:57:21 +0200

    message posting

--- assets/js/app/controllers/HomeController.js
+++ assets/js/app/controllers/HomeController.js
@@ -4,12 +4,26 @@
   angular.module('Twitter')
     .controller('HomeController', HomeController);
 
-  HomeController.$inject = ['$scope'];
-  function HomeController($scope) {
+  HomeController.$inject = ['$scope', 'Post'];
+  function HomeController($scope, Post) {
+    $scope.posts = Post.query();
+
     $scope.post = '';
     
     $scope.sendPost =function (post) {
-      console.log('post: ' + post);
+      let thisPost = new Post ({post: post});
+      
+      thisPost.$save().then(function (newPost) {
+        $scope.posts.push(newPost);
+        $scope.post = '';
+      })
+    }
+    
+    $scope.removePost = function (post, $index) {
+      if (!confirm('Are you sure?')) return;
+      post.$remove().then(function () {
+        $scope.posts.splice($index, 1);
+      })
     }
   }
 })();
